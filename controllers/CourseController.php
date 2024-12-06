db1a127b88331aa5726a03d9f57780cf9ba8d0a5
MhdHaekal <if23.muhamadhaekal@mhs.ubpkarawang.ac.id>
2024-12-06 20:30:24 +0700

    Add files via upload

--- controllers/CourseController.php
+++ controllers/CourseController.php
@@ -1,6 +1,6 @@
 <?php
 // Menggunakan jalur absolut untuk menghindari masalah path relatif
-include $_SERVER['DOCUMENT_ROOT'] . '../FINAL/config/db.php';
+include $_SERVER['DOCUMENT_ROOT'] . '../g/config/db.php';
 
 class CourseController {
 
